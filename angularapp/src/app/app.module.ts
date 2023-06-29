3911049141d0d723079a4883c381a64508b90661
ArunPrakash7 <arunnks5555@gmail.com>
2023-06-29 08:53:16 +0000

    loginpage

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingpageComponent,
+    AdminloginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
