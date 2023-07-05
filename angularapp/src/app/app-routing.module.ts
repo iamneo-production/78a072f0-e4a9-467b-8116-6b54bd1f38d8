f4dae7712cdf26a03b4be0208c0eb4c2be92a240
Saraswathi-Murugesan <saraswathim2452002@gmail.com>
2023-07-05 06:35:01 +0000

    Rooms-frontend

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoomcustomerComponent } from './roomcustomer/roomcustomer.component';
+import { RoomadminComponent } from './roomadmin/roomadmin.component';
+import { UpdateroomComponent } from './updateroom/updateroom.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:RoomcustomerComponent},
+  {path:"rooms/admin",component:RoomadminComponent},
+  {path:"rooms/customer",component:RoomcustomerComponent},
+  {path:"updateroom/:id",component:UpdateroomComponent},
+  { path: '', redirectTo: '/hotel-rooms', pathMatch: 'full' },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
