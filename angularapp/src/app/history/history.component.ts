4ba659f376c201e294dfcbfb65f390b4da11062b
397Bharathkumar <bharathkumar2822911@gmail.com>
2023-07-07 05:26:07 +0000

    created history

---
+++ angularapp/src/app/history/history.component.ts
@@ -0,0 +1,52 @@
+import { Component,OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-history',
+  templateUrl: './history.component.html',
+  styleUrls: ['./history.component.css']
+})
+export class HistoryComponent {
+  public rooms: any
+  filteredRooms: any[] = [];
+ constructor( private http:HttpClient){
+   this.filteredRooms = this.rooms;
+ }
+ ngOnInit(){
+   //this.x.getdata()
+       //.subscribe(data => this.rooms = data)
+  let response = this.http.get("assets/bookingdata.json");
+   response.subscribe((data) =>this.rooms = data);
+   //});
+ }
+ //searchText: FormControl = new FormControl('');
+     roomTypeFilter: FormControl = new FormControl('');
+   //roomPriceFilter: FormControl = new FormControl('');
+   
+ filterRooms() {
+   //const searchText = this.searchText.value.toLowerCase();
+   const roomTypeFilter = this.roomTypeFilter.value.toLowerCase();
+   //const roomPriceFilter = this.roomPriceFilter.value;
+ 
+   this.filteredRooms = this.rooms.filter((room: any) => {
+     //const matchesSearchText = room.type.toLowerCase().includes(searchText) ||
+       //room.description.toLowerCase().includes(searchText);
+ 
+     const matchesRoomTypeFilter = roomTypeFilter === '' || room.type.toLowerCase() === roomTypeFilter;
+ 
+     //const matchesRoomPriceFilter = roomPriceFilter === null || room.price <= roomPriceFilter;
+ 
+     return  matchesRoomTypeFilter //&& matchesRoomPriceFilter;
+   });
+ }
+ showContent: boolean = true;
+ 
+   onSearchFocus(): void {
+     this.showContent = false;
+   }
+ 
+   onSearchBlur(): void {
+     this.showContent = true;
+   }
+}
