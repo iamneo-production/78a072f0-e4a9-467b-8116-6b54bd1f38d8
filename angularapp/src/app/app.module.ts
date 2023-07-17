872b3b6129d312cb1f30eb1964210366723d8e16
397Bharathkumar <bharathkumar2822911@gmail.com>
2023-07-17 10:47:22 +0000

    added module.ts

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+
 import { HistoryComponent } from './history/history.component';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AdminhistoryComponent } from './adminhistory/adminhistory.component';
+AdminhistoryComponent
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { AdminhistoryComponent } from './adminhistory/adminhistory.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
