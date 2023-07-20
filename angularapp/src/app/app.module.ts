7dec7f40a12a4fef001e9c9ce62776fc72d8a87e
Saraswathi M <76590004+Saraswathi-Murugesan@users.noreply.github.com>
2023-07-21 01:17:06 +0530

    Update app.module.ts

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     RoomcustomerComponent,
     RoomadminComponent,
     UpdateroomComponent
-
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
