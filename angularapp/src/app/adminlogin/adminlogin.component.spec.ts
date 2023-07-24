7cf1708fa737ef091f067185151bee53c427f3e8
ArunPrakash7 <100114343+ArunPrakash7@users.noreply.github.com>
2023-07-24 21:55:42 +0530

    Login

--- angularapp/src/app/adminlogin/adminlogin.component.spec.ts
+++ angularapp/src/app/adminlogin/adminlogin.component.spec.ts
@@ -5,27 +5,17 @@ import { AdminloginComponent } from './adminlogin.component';
 describe('AdminloginComponent', () => {
   let component: AdminloginComponent;
   let fixture: ComponentFixture<AdminloginComponent>;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdminloginComponent ]
-    })
-    beforeEach(async () => {
-      await TestBed.configureTestingModule({
-        declarations: [ AdminloginComponent ]
-      })
-      .compileComponents();
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AdminloginComponent]
     });
     fixture = TestBed.createComponent(AdminloginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-beforeEach(() => {
-  fixture = TestBed.createComponent(AdminloginComponent);
-  component = fixture.componentInstance;
-  fixture.detectChanges();
-});
 
-it('should create', () => {
-  expect(component).toBeTruthy();
-});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,24 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AdminregisterComponent } from './adminregister/adminregister.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
+
+
+const routes:Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'adminlogin',component:AdminloginComponent},
+  {path:'adminregister',component:AdminregisterComponent},
+  {path:'',component:LandingpageComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     RegisterComponent,
+    LoginComponent,
     LandingpageComponent,
-    AdminloginComponent
+    AdminregisterComponent,
+    AdminloginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
