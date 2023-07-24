4cb7c1d3818662523922cb070523d0c93e7c819a
Saraswathi-Murugesan <saraswathim2452002@gmail.com>
2023-07-24 21:36:48 +0000

    updated backend-rooms

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,18 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CustomerComponent } from './customer/customer.component';
+
 
 @NgModule({
   declarations: [
-
     AppComponent,
     CancellationComponent,
     RoomcustomerComponent,
     RoomadminComponent,
-    UpdateroomComponent,
-    CustomerComponent
-  ],
+    UpdateroomComponent
+],
+
   imports: [
     BrowserModule,
     FormsModule,
