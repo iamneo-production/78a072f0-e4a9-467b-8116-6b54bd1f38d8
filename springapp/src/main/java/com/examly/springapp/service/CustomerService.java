81bdc46e2dcf402b41efeb7b6952e0e001be0ef0
jeevapradeesh <97111303+jeevapradeesh@users.noreply.github.com>
2023-07-25 21:23:36 +0530

    Add files via upload

--- springapp/src/main/java/com/examly/springapp/service/CustomerService.java
+++ springapp/src/main/java/com/examly/springapp/service/CustomerService.java
@@ -1,48 +1,15 @@
-package com.examly.springapp.service;
+package com.customer.Customer.service;
+
+import com.customer.Customer.model.Customer;
+import com.customer.Customer.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.examly.springapp.model.Customer;
-import com.examly.springapp.repository.CustomerRepo;
-
-
-
-import java.util.List;
-
 @Service
 public class CustomerService {
-
-    private final CustomerRepo customerRepo;
-
     @Autowired
-    public CustomerService(CustomerRepo customerRepo) {
-        this.customerRepo = customerRepo;
-    }
-
-    public Customer createCustomer(Customer customer) {
-        return customerRepo.save(customer);
-    }
-
-    public List<Customer> getAllCustomers() {
-        return customerRepo.findAll();
-    }
-
-    public Customer getCustomerById(Integer id) {
-        return customerRepo.findById(id).orElse(null);
-    }
-
-
-
-    public Customer updateCustomer(Integer id, Customer customer) {
-        if (customerRepo.existsById(id)) {
-            customer.setCustomerId(id);
-            return customerRepo.save(customer);
-        } else {
-            return null;
-        }
-    }
-
-    public void deleteCustomer(Integer id) {
-        customerRepo.deleteById(id);
-    }
+    CustomerRepository customerRepository;
+    public  Customer registerCustomer(Customer customer){
+         return customerRepository.save(customer);
+    };
 }
