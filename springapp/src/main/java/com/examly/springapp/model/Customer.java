41667744ba43f474e17baef70a71bb316fb68bf1
jeevapradeesh <97111303+jeevapradeesh@users.noreply.github.com>
2023-07-25 21:21:48 +0530

    Add files via upload

--- springapp/src/main/java/com/examly/springapp/model/Customer.java
+++ springapp/src/main/java/com/examly/springapp/model/Customer.java
@@ -1,78 +1,86 @@
-package com.examly.springapp.model;
+package com.customer.Customer.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Hari")
+@Table(name = "customer")
+@JsonIgnoreProperties(value = {"handler","hibernateLazyInitializer","FieldHandler"})
 public class Customer {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
-	
-	private int customerId;
-	
-	@Column(name="name")
-	private String name;
-	
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+        @Column(name = "name")
+    private String name;
+    @Column(name = "checkIn")
+    private String checkIn;
+    @Column(name = "checkOut")
+    private String checkOut;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phoneNumber")
+    private String phoneNumber;
 
-	
-	@Column(length = 255)
-	private String email;
-	
+    public  Customer(){
 
-	
-	@Column(length = 10)
-	private int phone;
+    }
+    public Customer(long id, String name, String checkIn, String checkOut, String email, String phoneNumber) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 
-	
+    public long getId() {
+        return id;
+    }
 
-	public Customer(){
-		super();
-	}
-	
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public  Customer( int customerId, String name,String email,
-			int phone ) {
-	    super();
-		this.customerId = customerId;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
+    public String getName() {
+        return name;
+    }
 
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getCustomerId() {
-		return this.customerId;
-	}
+    public String getCheckIn() {
+        return checkIn;
+    }
 
-	public void setCustomerId(int customerId) {
-		this.customerId = customerId;
-	}
+    public void setCheckIn(String checkIn) {
+        this.checkIn = checkIn;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getCheckOut() {
+        return checkOut;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return this.email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getPhone() {
-		return this.phone;
-	}
-	public void setPhone(int phone) {
-		this.phone = phone;
-	}	
+    public void setCheckOut(String checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
-
