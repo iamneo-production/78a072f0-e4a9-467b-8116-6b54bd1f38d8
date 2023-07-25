0f8cb9c980428a521afd72964373e3925fea6a0a
jeevapradeesh <97111303+jeevapradeesh@users.noreply.github.com>
2023-07-25 20:03:44 +0530

    Add files via upload

---
+++ angularapp/src/src/app/room.ts
@@ -0,0 +1,11 @@
+export class Room {
+    roomNumber   !:number;
+    category     ?:String;
+    roomType     ?:any;
+    arrivalDate  ?:Date;
+    departDate   ?:Date;
+    noOfGuests   ?:number;
+    price        ?:number;
+    availability ?:number;;
+ }
+ 
