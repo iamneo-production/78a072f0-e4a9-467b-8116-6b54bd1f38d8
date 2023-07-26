49367fab3908e26efce2493fb4e6590a422e15ef
Saraswathi-Murugesan <saraswathim2452002@gmail.com>
2023-07-26 08:36:36 +0000

    commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -13,6 +13,13 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+const routes: Routes = [
+  {path:"",component:RoomcustomerComponent},
+  {path:"roomadmin",component:RoomadminComponent},
+  {path:"roomcustomer",component:RoomcustomerComponent},
+  {path:"updateroom",component:UpdateroomComponent} 
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +28,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     UpdateroomComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
