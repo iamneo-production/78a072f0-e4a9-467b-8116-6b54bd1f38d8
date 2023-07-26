f9a82dd995bedf7c08490f643270295c6b67fffb
jeevapradeesh <jeeva23oct1@gmail.com>
2023-07-26 04:53:18 +0000

    commit

---
+++ springapp/src/main/java/com/examly/springapp/service/PaymentService.java
@@ -0,0 +1,17 @@
+package com.examly.springapp.service;
+import com.examly.springapp.model.Payment;
+import com.examly.springapp.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PaymentService {
+
+
+        @Autowired
+        public PaymentService(PaymentRepository paymentRepository) {
+            this.paymentRepository = paymentRepository;
+        }
+    }
