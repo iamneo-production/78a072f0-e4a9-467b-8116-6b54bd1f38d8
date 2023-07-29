81ab262bb10425cc79d4b3e2dfa5dc3c219dbab2
Harinagi03 <naginenihari5336@gmail.com>
2023-07-29 12:43:34 +0000

    changes

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { RoomcustomerComponent } from './roomcustomer/roomcustomer.component';
 import { RoomadminComponent } from './roomadmin/roomadmin.component';
 import { UpdateroomComponent } from './updateroom/updateroom.component';
 import { CustomerComponent } from './customer/customer.component';
+import { CancellationComponent } from './cancellation/cancellation.component';
 
 const routes: Routes = [
   {path: 'component', component: CustomerComponent},
@@ -12,15 +12,9 @@ const routes: Routes = [
   {path:"admin/rooms",component:RoomadminComponent},
   {path:"rooms",component:RoomcustomerComponent},
   {path:"updateroom/:id",component:UpdateroomComponent},
-  {path: '', redirectTo: '/hotel-rooms', pathMatch: 'full' },  
+  {path: '', redirectTo: '/hotel-rooms', pathMatch: 'full' },
+  {path:'bookings/:bookingId/cancellations',component:CancellationComponent},  
 ];
-=======
-import { CancellationComponent } from './cancellation/cancellation.component';
-import { CustomerComponent } from './customer/customer.component';
-
-const routes: Routes = [{path:'bookings/:bookingId/cancellations',component:CancellationComponent},
-{path: 'component', component: CustomerComponent}];
->>>>>>> 1fa8947874b612d1a794b45ec05fb4d5bd764111
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RoomcustomerComponent } from './roomcustomer/roomcustomer.component';
 import { RoomadminComponent } from './roomadmin/roomadmin.component';
 import { UpdateroomComponent } from './updateroom/updateroom.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomerComponent } from './customer/customer.component';
+import { CancellationComponent } from './cancellation/cancellation.component';
 
 
 
@@ -20,32 +20,17 @@ import { CustomerComponent } from './customer/customer.component';
     RoomcustomerComponent,
     RoomadminComponent,
     UpdateroomComponent,
-
-=======
-import { CancellationComponent } from './cancellation/cancellation.component';
-import { CustomerComponent } from './customer/customer.component';
-import { HttpClientModule } from '@angular/common/http';
-@NgModule({
-  declarations: [
-    AppComponent,
     CancellationComponent,
-    CustomerComponent
->>>>>>> 1fa8947874b612d1a794b45ec05fb4d5bd764111
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule,
     FormsModule,
     NoopAnimationsModule,
     
 ],
-=======
-    FormsModule,
-    HttpClientModule
-  ],
->>>>>>> 1fa8947874b612d1a794b45ec05fb4d5bd764111
   providers: [],
   bootstrap: [AppComponent]
 })
